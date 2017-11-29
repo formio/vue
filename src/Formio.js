c2118afcea096500fbac09f69367b0aa5eba43ab
Randall Knutson <randall@form.io>
2017-11-29 13:57:19 -0600

    Finish and test out vue wrapper.

--- src/Formio.js
+++ src/Formio.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
-import { FormioFactory } from 'formiojs/factory';
+import {Formio as FormioFull} from 'formiojs/full';
 
-export const Formio = {
+const Formio = {
+  name: 'formio',
   props: {
     src: {
       type: String
@@ -14,59 +15,112 @@ export const Formio = {
     },
     options: {
       type: Object,
-      default: {}
+      default: () => ({})
     }
   },
 
   mounted() {
-    if (this.src) {
-      this.createPromise = FormioFactory.createForm(this.$refs.formio, this.src, this.options).then(formio => this.formio = formio);
+    this.initializeForm()
+      .then(() => {
+        this.setupForm();
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+  },
+
+  destroyed() {
+    if (this.formio) {
+      this.formio.destroy(true);
     }
-    if (this.form) {
-      this.createPromise = FormioFactory.createForm(this.$refs.formio, this.form, this.options).then(formio => this.formio = formio);
-    }
-    this.initializeFormio();
+  },
+
+  methods: {
+    initializeForm() {
+      return new Promise((resolve, reject) => {
+        if (this.src) {
+          resolve(FormioFull.createForm(this.$refs.formio, this.src, this.options)
+            .then(formio => {
+              this.formio = formio;
+              return formio;
+            }))
+            .catch(err => {
+              console.error(err);
+            });
+        }
+        else if (this.form) {
+          resolve(FormioFull.createForm(this.$refs.formio, this.form, this.options)
+            .then(formio => {
+              this.formio = formio;
+              return formio;
+            }));
+        }
+        else {
+          // If we get to here there is no src or form
+          reject('Must set src or form attribute');
+        }
+      });
+    },
+
+    setupForm() {
+      if (this.submission) {
+        this.formio.submission = this.submission
+      }
+
+      if (this.url) {
+        this.formio.url = this.url
+      }
+
+      this.formio.events.onAny((...args) => {
+        const eventParts = args[0].split('.');
+
+        // Only handle formio events.
+        if (eventParts[0] !== 'formio' || eventParts.length !== 2) {
+          return;
+        }
+
+        // Remove formio. from event.
+        args[0] = eventParts[1];
+
+        this.$emit.apply(this, args);
+
+        // Emit custom events under their own name as well.
+        if (eventParts[1] === 'customEvent') {
+          args[0] = args[1].type;
+          this.$emit.apply(this, args);
+        }
+      });
+    },
   },
 
   watch: {
     src: function(value) {
-      this.createPromise = FormioFactory.createForm(this.element, value, this.options).then(formio => this.formio = formio);
-      this.initializeFormio();
+      if (this.formio) {
+        this.formio.src = value;
+      }
+    },
+    url: function(value) {
+      if (this.formio) {
+        this.formio.url = value;
+      }
     },
     form: function(value) {
-      this.createPromise = FormioFactory.createForm(this.element, value, this.options).then(formio => this.formio = formio);
-      this.initializeFormio();
+      if (this.formio) {
+        this.formio.form = value;
+      }
     },
     submission: function(value) {
-      this.formio.submission = value;
+      if (this.formio) {
+        this.formio.submission = value;
+      }
     }
   },
 
-  initializeFormio() {
-    if (this.createPromise) {
-      this.createPromise.then(() => {
-        if (this.submission) {
-          this.formio.submission = this.submission;
-        }
-        this.formio.on('prevPage', this.emit('prevPage'));
-        this.formio.on('nextPage', this.emit('nextPage'));
-        this.formio.on('change', this.emit('change'));
-        this.formio.on('customEvent', this.emit('customEvent'));
-        this.formio.on('submit', this.emit('submit'));
-        this.formio.on('submitDone', this.emit('submitDone'));
-        this.formio.on('error', this.emit('error'));
-        this.formio.on('render', this.emit('render'));
-      });
-    }
-  },
-
-  emit(eventName) {
-    return (...args) => {
-      this.$emit(eventName, ...args);
-    }
-  },
-
-  template: '<div ref="formio" />'
+  render(createElement) {
+    return createElement('div', {ref: 'formio'});
+  }
 };
+
+export default Formio;
 
 Vue.component('formio', Formio);
